5df10833e0087ae613c2f481963eb964e773411b
Areum Choi <reumaco99@gmail.com>
2022-06-30 15:22:43 +0900

    Feat: DrainpipeMonitoringModule 제작 (#5)
    * Feat: "Add Drainpipe-monitoring Module"
    
    하수관로 수위 현황 API 데이터를 가져오기 위한 모듈 생성
    
    클라이언트 요청 Dto - get.drainpipe-info.dto (구분코드, 측정일자1, 측정일자2 요청인자)
    
    서버 응답 Dto - response.drainpipe-info.dto (row-출력값 6개 항목)
    
    현재 데이터 키값 "row"에 해당하는 건수들 받아와서 확인 완료.
    
    클라이언트 요청 매개변수 추가 필요.
    Dto에 맞게 응답값 데이터 파싱 필요.
    
    Fixes: #3
    
    * Feat: "Add 클라이언트 요청에 따른 응답값 전달"
    
    기존 get.drainpipi..dto 파일명, 클래스명 변경
    
    기존 response.drainpipi..dto 클래스명 변경
    
    클라이언트 json 데이터 요청에 따른 수정
    -컨트롤러 클래스 getDrainInfo에 파라미터 추가
    -서비스 클래스내 getDrainpipeApi 파라미터 추가 및 응답데이터 ResponseDrainpipeInfoDto 데이터 파싱
    
    Resolves: #4
    
    * Feat: 데이터 총 개수에 따른 정보 조회 관련 추가
    
    데이터 조회시 요청값으로 첫번째 인덱스와 마지막 인덱스를 기본설정을 해둔 상태로 전체 데이터 조회가 안됐었음.
    
    <Service>
    -getDataTotalCount() 데이터 행 전체 개수 구하기 추가
    -rowData() 데이터에서 행만 가져오기 추가
    -getApiData() api 데이터 가져오기 추가
    
    <Dto>
    -Dto폴더 추가하고 dto 파일 이동
    -DrainpipeInfoOpenApiDto 클래스 추가
    -RequestDrainpipeInfoDto 변수명 대문자로 변경
    -ResponseDrainpipeInfoDto 변수명 대문자로 변경
    
    Resolves: #4
    
    * Fix: DrainpipeMonitoringService 내 오타 수정
    
     DrainpipeMonitoringService 클래스 파일 51번째 줄에 부등호 오타 변경
    
    getDataTotalCount() 삭제
    
    Promise.all()을 사용하려고 했으나 실패
    
    Co-authored-by: Hyunseok Ko <56003992+khsexk@users.noreply.github.com>

---
+++ src/drainpipe-monitoring/drainpipe-monitoring.service.ts
@@ -0,0 +1,105 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
+import { firstValueFrom, map } from 'rxjs';
+import { ResponseDrainpipeInfoDto } from './dto/response.drainpipe-info.dto';
+import { DrainpipeInfoOpenApiDto } from './dto/drainpipe-info-openapi.dto';
+
+@Injectable()
+export class DrainpipeMonitoringService {
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async getDrainpipeApi(
+    reqGubn,
+    reqYmd,
+    reqYmd2,
+  ): Promise<ResponseDrainpipeInfoDto[]> {
+    //데이터 총 개수
+    const totalDataCount = await this.getApiData(
+      'count',
+      1,
+      1,
+      reqGubn,
+      reqYmd,
+      reqYmd2,
+    );
+    //리턴할 새 배열 생성
+    const responseArr = [];
+
+    //최대 1000건 요청을 위해 데이터 자르기
+    const repeat = totalDataCount / 1000;
+
+    //1000개 단위 데이터 요청
+    for (let i = 0; i <= repeat; i++) {
+      const getData = await this.rowData(
+        i * 1000 + 1,
+        (i + 1) * 1000,
+        reqGubn,
+        reqYmd,
+        reqYmd2,
+      );
+
+      for (const idx in getData) {
+        responseArr.push(getData[idx]);
+      }
+    }
+
+    //1000개 단위로 나눈 나머지 요청
+    if (totalDataCount > repeat * 1000) {
+      const getData = await this.rowData(
+        repeat * 1000 + 1,
+        totalDataCount,
+        reqGubn,
+        reqYmd,
+        reqYmd2,
+      );
+
+      for (const idx in getData) {
+        responseArr.push(getData[idx]);
+      }
+    }
+    return responseArr;
+  }
+
+  //각 행 가져오기
+  async rowData(startIdx, lastIdx, reqGubn, reqYmd, reqYmd2) {
+    const data = await this.getApiData(
+      'row',
+      startIdx,
+      lastIdx,
+      reqGubn,
+      reqYmd,
+      reqYmd2,
+    );
+    return data;
+  }
+
+  //OPEN API 데이터 가져오기
+  async getApiData(
+    tag,
+    startIdx,
+    lastIdx,
+    reqGubn,
+    reqYmd,
+    reqYmd2,
+  ): Promise<any> {
+    const key = this.configService.get<string>('OPEN_API_KEY');
+    //const testUrl = `http://openapi.seoul.go.kr:8088/${key}/json/DrainpipeMonitoringInfo/1/5/01/2022030214/2022030315`;
+    const url = `http://openapi.seoul.go.kr:8088/${key}/json/DrainpipeMonitoringInfo/${startIdx}/${lastIdx}/${reqGubn}/${reqYmd}/${reqYmd2}`;
+
+    const responseData = await firstValueFrom(
+      this.httpService
+        .get<DrainpipeInfoOpenApiDto>(url)
+        .pipe(map((response) => response.data)),
+    );
+
+    if (tag === 'row') {
+      return responseData.DrainpipeMonitoringInfo.row;
+    } else if (tag === 'count') {
+      return responseData.DrainpipeMonitoringInfo.list_total_count;
+    }
+  }
+}
---
+++ src/drainpipe-monitoring/dto/response.drainpipe-info.dto.ts
@@ -0,0 +1,8 @@
+export class ResponseDrainpipeInfoDto {
+  IDN: string;
+  GUBN: string;
+  GUBN_NAM: string;
+  MEA_YMD: string;
+  MEA_WAL: number;
+  SIG_STA: string;
+}
