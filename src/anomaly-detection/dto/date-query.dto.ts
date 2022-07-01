070d434f33e7d7ec4c2e08f2f11cabb4b41f4441
Hyunseok Ko <56003992+khsexk@users.noreply.github.com>
2022-07-01 18:13:11 +0900

    Docs: readme 파일 작성 (#16)
    * Fix: 정규식 변경
    
    * Docs: readme 작성 및 resouces
    
    * Docs: Readme 파일 수정

--- src/anomaly-detection/dto/date-query.dto.ts
+++ src/anomaly-detection/dto/date-query.dto.ts
@@ -3,13 +3,14 @@ import { IsNotEmpty, Matches } from 'class-validator';
 
 export class DateQuery {
   @ApiProperty()
-  @Matches(/\d{4}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-9]|2[0-4])/)
+
+  @Matches(/\d{4}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(0[0-9]|1[0-9]|2[0-4])/)
   @IsNotEmpty()
   //yyyymmddhh
   start!: string;
 
   @ApiProperty()
-  @Matches(/\d{4}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(0[1-9]|1[0-9]|2[0-4])/)
+  @Matches(/\d{4}(0[1-9]|1[012])(0[1-9]|[12][0-9]|3[01])(0[0-9]|1[0-9]|2[0-4])/)
   @IsNotEmpty()
   end!: string;
 }
